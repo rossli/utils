dbf6347125f1556841c1b29cb61b8dfd55b1dfe9
rachelroll <hai30@163.com>
2019-05-21 19:09:15 +0800

    Update send sms

--- Utils.php
+++ Utils.php
@@ -9,6 +9,7 @@
 namespace App\Utils;
 
 use Hashids\Hashids;
+use phpDocumentor\Reflection\Types\Self_;
 
 class Utils
 {
@@ -17,7 +18,6 @@ class Utils
     {
         return preg_match("/^1[0-9]{2}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/", $string);
     }
-
 
     /**
      * @param        $url
@@ -31,60 +31,66 @@ class Utils
      * @requestGetExample $data = Utils::curl("https://api.ipify.org");
      * @requestPostExample
      * $CurlPOST = Utils::curl("http://jsonplaceholder.typicode.com/posts", $method = "POST", $data = array(
-    "title"  => 'foo',
-    "body"   => 'bar',
-    "userId" => 1,
-    ));
+     * "title"  => 'foo',
+     * "body"   => 'bar',
+     * "userId" => 1,
+     * ));
      * @authCurlExample
      * $curlBasicAuth = Utils::curl(
-    "http://jsonplaceholder.typicode.com/posts",
-    $method = "GET",
-    $data = false,
-    $header = false,
-    $returnInfo = false,
-    $auth = array(
-    'username' => 'your_login',
-    'password' => 'your_password',
-    )
-    );
+     * "http://jsonplaceholder.typicode.com/posts",
+     * $method = "GET",
+     * $data = false,
+     * $header = false,
+     * $returnInfo = false,
+     * $auth = array(
+     * 'username' => 'your_login',
+     * 'password' => 'your_password',
+     * )
+     * );
      * @CustomHeaders:
-     *  $curlWithHeaders = Recipe::curl("http://jsonplaceholder.typicode.com/posts", $method = "GET", $data = false, $header = array(
-    "Accept" => "application/json",
-    ), $returnInfo = true);
+     *  $curlWithHeaders = Recipe::curl("http://jsonplaceholder.typicode.com/posts", $method = "GET", $data = false,
+     *     $header = array(
+     * "Accept" => "application/json",
+     * ), $returnInfo = true);
      */
-    public static function curl($url, $method = 'GET', $data = false, $headers = false, $returnInfo = false, $auth = false)
-    {
+    public static function curl($url,
+        $method = 'GET',
+        $data = FALSE,
+        $headers = FALSE,
+        $returnInfo = FALSE,
+        $auth = FALSE
+    ) {
         $ch = curl_init();
-        $info = null;
+        $info = NULL;
         if (strtoupper($method) == 'POST') {
             curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            if ($data !== false) {
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            if ($data !== FALSE) {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             }
         } else {
-            if ($data !== false) {
+            if ($data !== FALSE) {
                 if (is_array($data)) {
                     $dataTokens = [];
                     foreach ($data as $key => $value) {
-                        array_push($dataTokens, urlencode($key).'='.urlencode($value));
+                        array_push($dataTokens, urlencode($key) . '=' . urlencode($value));
                     }
                     $data = implode('&', $dataTokens);
                 }
-                curl_setopt($ch, CURLOPT_URL, $url.'?'.$data);
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $data);
             } else {
                 curl_setopt($ch, CURLOPT_URL, $url);
             }
         }
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        if ($headers !== false) {
+        if ($headers !== FALSE) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
-        if ($auth !== false && strlen($auth['username']) > 0 && strlen($auth['password']) > 0) {
-            curl_setopt($ch, CURLOPT_USERPWD, $auth['username'].':'.$auth['password']);
+        if ($auth !== FALSE && strlen($auth['username']) > 0 && strlen($auth['password']) > 0) {
+            curl_setopt($ch, CURLOPT_USERPWD, $auth['username'] . ':' . $auth['password']);
         }
         $contents = curl_exec($ch);
         if ($returnInfo) {
@@ -94,6 +100,7 @@ class Utils
         if ($returnInfo) {
             return ['contents' => $contents, 'info' => $info];
         }
+
         return $contents;
     }
 
@@ -103,35 +110,79 @@ class Utils
         *	自动添加链接前面的 http://
         *	$slash 是判断是否要后面添加斜杠
         */
-    public static function format_url($url,$slash=false)
+    public static function format_url($url, $slash = FALSE)
     {
 
-        if (substr($url,0,4)!='http') {
-            @$if_https = $_SERVER['HTTPS'];	//这样就不会有错误提示
-            if ($if_https) {	//如果是使用 https 访问的话就添加 https
-                $url='https:'.$url;
+        if (substr($url, 0, 4) != 'http') {
+            @$if_https = $_SERVER['HTTPS'];    //这样就不会有错误提示
+            if ($if_https) {    //如果是使用 https 访问的话就添加 https
+                $url = 'https:' . $url;
             } else {
-                $url='http:'.$url;
+                $url = 'http:' . $url;
             }
         }
         if ($slash) {
-            $url = rtrim($url,'/').'/';
+            $url = rtrim($url, '/') . '/';
         }
+
         return $url;
     }
 
     /**
      * 去掉所有的空格
+     *
      * @param $_str
      *
      * @return string|string[]|null
      */
     public static function trim($_str)
     {
-        return $_str =preg_replace("/\s/","",$_str);
+        return $_str = preg_replace("/\s/", "", $_str);
 
     }
 
+    /**
+     * @param        $phone
+     * @param        $msg
+     * @param bool   $report
+     * @param int    $send_time
+     * @param string $uid
+     *
+     * {
+     * "account" : "N6000001", //用户在253云通讯平台上申请的API账号
+     * "password" : "123456", //用户在253云通讯平台上申请的API账号对应的API密钥
+     * "msg" : "【253】您的验证码是：2530", //短信内容。长度不能超过536个字符
+     * "phone" : "15800000000", //手机号码。多个手机号码使用英文逗号分隔
+     * "sendtime" : "201704101400", //定时发送短信时间。格式为yyyyMMddHHmm，值小于或等于当前时间则立即发送，不填则默认为立即发送（选填参数）
+     * "report" : "true", //是否需要状态报告（默认为false）（选填参数）
+     * "extend" : "555", //用户自定义扩展码，纯数字，建议1-3位（选填参数）
+     * "uid" : "批次编号-场景名（英文或者拼音）" //自助通系统内使用UID判断短信使用的场景类型，可重复使用，可自定义场景名称，示例如 VerificationCode（选填参数）
+     * }
+     */
+    public static function sendSms253($phone, $msg, $report = TRUE, $send_time = 0, $uid = 'VerificationCode')
+    {
+        $url = 'http://smssh1.253.com/msg/send/json';
+        $account = env('SMS_ACCOUNT_253');
+        $password = env('SMS_PASSWORD_253');
+        $data = [
+            'account' => $account,
+            'password' => $password,
+            'phone' => $phone,
+            'msg' => '【师大教科文】' . $msg,
+            'report' => $report,
+            'sendtime' => $send_time,
+            'uid' => $uid,
+        ];
+        info('sms_data', $data);
 
+        $res = self::curl($url,'POST',$data);
+        if ($res['code'] === 0) {
+            return TRUE;
+        }
+
+        info('sms_error:' . json_encode($res));
+
+        return FALSE;
+    }
 
 }
