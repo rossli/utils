712da6192d5a5478aa7c58f973374b56e0cec045
rachelroll <hai30@163.com>
2020-03-16 11:04:12 +0800

    update(cut)

--- Utils.php
+++ Utils.php
@@ -379,16 +379,16 @@ class Utils
     }
 
     // 截取文章的一部分内容
-    public static function cutArticle($data, $str="....")
+    public static function cutArticle($data, $str = '...', $percent = 3 / 10)
     {
-        $data=strip_tags($data);//去除html标记
-        $pattern = "/&[a-zA-Z]+;/";//去除特殊符号
-        $data=preg_replace($pattern,'',$data);
+        $data = strip_tags($data);//去除html标记
+        $pattern = '/&[a-zA-Z]+;/';//去除特殊符号
+        $data = preg_replace($pattern, '', $data);
 
         // 设置只加载三分之一的内容
-        $cut = strlen($data) * 3 / 10;
+        $cut = strlen($data) * $percent;
 
-        $data = mb_strimwidth($data,0, $cut, $str);
+        $data = mb_strimwidth($data, 0, $cut, $str);
 
         return $data;
     }
