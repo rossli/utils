b11b3a934bc4434a140167cd1893275b3fea73dc
rossli <hai30@126.com>
2020-10-16 15:28:23 +0800

    update(): image radius

--- Utils.php
+++ Utils.php
@@ -17,6 +17,7 @@ class Utils
     /**
      * 手机号码验证 (非严格)
      * 更新时间 2019-03-06
+     *
      * @param $string
      *
      * @return false|int
@@ -56,14 +57,14 @@ class Utils
         // 根据前17位计算校验码
         $total = 0;
         for ($i = 0; $i < 17; $i++) {
-            $total += substr($idcard_base, $i, 1) * $factor[$i];
+            $total += substr($idcard_base, $i, 1) * $factor[ $i ];
         }
 
         // 取模
         $mod = $total % 11;
 
         // 比较校验码
-        if ($verify_code == $verify_code_list[$mod]) {
+        if ($verify_code == $verify_code_list[ $mod ]) {
             return TRUE;
         } else {
             return FALSE;
@@ -73,10 +74,10 @@ class Utils
     /**
      * @param        $url
      * @param string $method
-     * @param bool $data
-     * @param bool $headers
-     * @param bool $returnInfo
-     * @param bool $auth
+     * @param bool   $data
+     * @param bool   $headers
+     * @param bool   $returnInfo
+     * @param bool   $auth
      *
      * @return array|mixed
      * @requestGetExample $data = Utils::curl("https://api.ipify.org");
@@ -106,13 +107,12 @@ class Utils
      * ), $returnInfo = true);
      */
     public static function curl($url,
-                                $method = 'GET',
-                                $data = FALSE,
-                                $headers = FALSE,
-                                $returnInfo = FALSE,
-                                $auth = FALSE
-    )
-    {
+        $method = 'GET',
+        $data = FALSE,
+        $headers = FALSE,
+        $returnInfo = FALSE,
+        $auth = FALSE
+    ) {
         $ch = curl_init();
         $info = NULL;
         if (strtoupper($method) == 'POST') {
@@ -197,8 +197,8 @@ class Utils
     /**
      * @param        $phone
      * @param        $msg
-     * @param bool $report
-     * @param int $send_time
+     * @param bool   $report
+     * @param int    $send_time
      * @param string $uid
      *
      * {
@@ -218,13 +218,13 @@ class Utils
         $account = env('SMS_ACCOUNT_253');
         $password = env('SMS_PASSWORD_253');
         $data = [
-            'account' => $account,
+            'account'  => $account,
             'password' => $password,
-            'phone' => $phone,
-            'msg' => '【师大教科文】' . $msg,
-            'report' => $report,
+            'phone'    => $phone,
+            'msg'      => '【师大教科文】' . $msg,
+            'report'   => $report,
             'sendtime' => $send_time,
-            'uid' => $uid,
+            'uid'      => $uid,
         ];
         //开发环境不发短信
         if (env('APP_DEBUG_SMS')) {
@@ -239,9 +239,9 @@ class Utils
         info('sms_send_res:' . json_encode($res));
         if ($res['code'] == 0) {
             info('sms_send_ok');
+
             return TRUE;
         }
-
 
         return FALSE;
     }
@@ -267,15 +267,15 @@ class Utils
     /**
      * 将一个字符串部分字符用$re替代隐藏
      *
-     * @param string $string 待处理的字符串
-     * @param int $start 规定在字符串的何处开始，
+     * @param string $string      待处理的字符串
+     * @param int    $start       规定在字符串的何处开始，
      *                            正数 - 在字符串的指定位置开始
      *                            负数 - 在从字符串结尾的指定位置开始
      *                            0 - 在字符串中的第一个字符处开始
-     * @param int $length 可选。规定要隐藏的字符串长度。默认是直到字符串的结尾。
+     * @param int    $length      可选。规定要隐藏的字符串长度。默认是直到字符串的结尾。
      *                            正数 - 从 start 参数所在的位置隐藏
      *                            负数 - 从字符串末端隐藏
-     * @param string $re 替代符
+     * @param string $re          替代符
      *
      * @return string   处理后的字符串
      */
@@ -300,7 +300,7 @@ class Utils
             $end -= abs($length);
         }
         for ($i = $begin; $i <= $end; $i++) {
-            $strarr[$i] = $re;
+            $strarr[ $i ] = $re;
         }
         if ($begin >= $end || $begin >= $last || $end > $last) {
             return FALSE;
@@ -312,7 +312,7 @@ class Utils
     public static function parseUrl($url, $query)
     {
         $parsedUrl = parse_url($url);
-        if (!isset($parsedUrl['path']) || $parsedUrl['path'] == null) {
+        if (!isset($parsedUrl['path']) || $parsedUrl['path'] == NULL) {
             $url .= '/';
         }
         $separator = (!isset($parsedUrl['query'])) ? '?' : '&';
@@ -350,24 +350,107 @@ class Utils
         $randstr = '';
         for ($i = 0; $i < $length; $i++) {
             $num = mt_rand(0, $len);
-            $randstr .= $str[$num];
+            $randstr .= $str[ $num ];
         }
+
         return $randstr;
     }
 
     public static function birthday($card_id)
     {
-        if (empty($card_id)) return null;
+        if (empty($card_id)) {
+            return NULL;
+        }
         $bir = substr($card_id, 10, 4);
+
         return $bir;
     }
 
     public static function unicodeDecode($sting)
     {
-        $json = '{"str":"'.$sting.'"}';
-        $arr = json_decode($json,true);
-        if(empty($arr)) return '';
+        $json = '{"str":"' . $sting . '"}';
+        $arr = json_decode($json, TRUE);
+        if (empty($arr)) {
+            return '';
+        }
+
         return $arr['str'];
+    }
+
+    /**
+     * 图片加圆角
+     * $outPath 这里到具体的图片类型,一般PNG
+     *
+     * @param     $imgPath
+     * @param     $outPath
+     * @param int $radius
+     *
+     * @return bool
+     */
+    public static function radiusImg($imgPath, $outPath, $radius = 0)
+    {
+        $ext = pathinfo($imgPath);
+        $src_img = NULL;
+
+        switch ($ext['extension']) {
+            case 'jpg':
+                $src_img = imagecreatefromjpeg($imgPath);
+                break;
+            case 'png':
+                $src_img = imagecreatefrompng($imgPath);
+                break;
+        }
+
+        $wh = getimagesize($imgPath);
+        $w = $wh[0];
+        $h = $wh[1];
+        $radius = $radius == 0 ? (min($w, $h) / 2) : $radius;
+        $img = imagecreatetruecolor($w, $h);
+        //这一句一定要有
+        imagesavealpha($img, TRUE);
+        //拾取一个完全透明的颜色,最后一个参数127为全透明
+        $bg = imagecolorallocatealpha($img, 255, 255, 255, 127);
+        imagefill($img, 0, 0, $bg);
+        $r = $radius; //圆 角半径
+        for ($x = 0; $x < $w; $x++) {
+            for ($y = 0; $y < $h; $y++) {
+                $rgbColor = imagecolorat($src_img, $x, $y);
+                if (($x >= $radius && $x <= ($w - $radius)) || ($y >= $radius && $y <= ($h - $radius))) {
+                    //不在四角的范围内,直接画
+                    imagesetpixel($img, $x, $y, $rgbColor);
+                } else {
+                    //在四角的范围内选择画
+                    //上左
+                    $y_x = $r;      //圆心X坐标
+                    $y_y = $r;      //圆心Y坐标
+                    if (((($x - $y_x) * ($x - $y_x) + ($y - $y_y) * ($y - $y_y)) <= ($r * $r))) {
+                        imagesetpixel($img, $x, $y, $rgbColor);
+                    }
+                    //上右
+                    $y_x = $w - $r; //圆心X坐标
+                    $y_y = $r;      //圆心Y坐标
+                    if (((($x - $y_x) * ($x - $y_x) + ($y - $y_y) * ($y - $y_y)) <= ($r * $r))) {
+                        imagesetpixel($img, $x, $y, $rgbColor);
+                    }
+                    //下左
+                    $y_x = $r;      //圆心X坐标
+                    $y_y = $h - $r; //圆心Y坐标
+                    if (((($x - $y_x) * ($x - $y_x) + ($y - $y_y) * ($y - $y_y)) <= ($r * $r))) {
+                        imagesetpixel($img, $x, $y, $rgbColor);
+                    }
+                    //下右
+                    $y_x = $w - $r; //圆心X坐标
+                    $y_y = $h - $r; //圆心Y坐标
+                    if (((($x - $y_x) * ($x - $y_x) + ($y - $y_y) * ($y - $y_y)) <= ($r * $r))) {
+                        imagesetpixel($img, $x, $y, $rgbColor);
+                    }
+                }
+            }
+        }
+        //header("content-type:image/jpeg");
+        imagepng($img, $outPath);
+
+        return TRUE;
     }
 
 }
